0d238775096c7d4635ec67bda8ca4e1b265972b9
Irina Krakavetskaya <ikrakavetskaya@elinext.com>
2024-03-15 11:48:34 +0300

    handled cookies and headers via middleware

--- next/src/middleware.js
+++ next/src/middleware.js
@@ -1,8 +1,46 @@
+// define Middleware in the root of your project, at the same level as pages or app, or inside src
 import { NextResponse } from 'next/server'
 
 export function middleware(request) {
-    return NextResponse.redirect(new URL('/dashboard', request.url))
+    // Getting cookies from the request using the `RequestCookies` API
+    let cookie = request.cookies.get('laravel_session')
+    // !!! all console.log info can be checked on terminal where npm run dev
+    console.log(cookie)
+    const allCookies = request.cookies.getAll()
+    console.log(allCookies)
+
+    request.cookies.has('laravel_session') // => true
+    request.cookies.delete('laravel_session')
+    request.cookies.has('laravel_session') // => false
+
+    // Setting cookies on the response using the `ResponseCookies` API
+    const response = NextResponse.next()
+    response.cookies.set('test', '1')
+    response.cookies.set({
+        name: 'test',
+        value: '2',
+        path: '/',
+    })
+    cookie = response.cookies.get('test')
+    console.log(cookie) // => { name: 'test', value: '2', Path: '/' }
+    // The outgoing response will have a `Set-Cookie:test=fast;path=/` header.
+
+    // Clone the request headers and set a new header `x-next-app-version`
+    const requestHeaders = new Headers(request.headers);
+    requestHeaders.set('x-next-app-version', '1.0');
+
+    // Set a new response header `x-hello-from-middleware2`
+    response.headers.set('x-laravel-app-version', '3.0');
+
+    //only for training
+    if (request.nextUrl.pathname.startsWith('/dashboard')) {
+        //return NextResponse.rewrite(new URL('/invoices', request.url))
+        //or
+        //return NextResponse.redirect(new URL('/invoices', request.url))
+    }
 }
+
+
 
 export const config = {
     // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
